b20b6bad1f455d2c129b58ef715bde72c613cea1
benja4510 <benjamindiaz4518@gmail.com>
2025-09-08 21:56:15 -0300

    Agregar archivos de proyecto.

---
+++ pryAgendaContactoBenjaminDiaz/GestionAgenda.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace pryAgendaContactoBenjaminDiaz
+{
+    partial class frmGestionAgenda
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblContacto = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.mtbNumero = new System.Windows.Forms.MaskedTextBox();
+            this.txtContacto = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.lstResultado = new System.Windows.Forms.ListBox();
+            this.lblCantidadCon = new System.Windows.Forms.Label();
+            this.lblFechayHora = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblContacto
+            // 
+            this.lblContacto.AutoSize = true;
+            this.lblContacto.Location = new System.Drawing.Point(66, 37);
+            this.lblContacto.Name = "lblContacto";
+            this.lblContacto.Size = new System.Drawing.Size(50, 13);
+            this.lblContacto.TabIndex = 0;
+            this.lblContacto.Text = "Contacto";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(66, 83);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(44, 13);
+            this.lblNumero.TabIndex = 1;
+            this.lblNumero.Text = "Numero";
+            // 
+            // mtbNumero
+            // 
+            this.mtbNumero.Location = new System.Drawing.Point(189, 83);
+            this.mtbNumero.Mask = "(351)000-0000";
+            this.mtbNumero.Name = "mtbNumero";
+            this.mtbNumero.Size = new System.Drawing.Size(84, 20);
+            this.mtbNumero.TabIndex = 2;
+            this.mtbNumero.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtbNumero_MaskInputRejected);
+            // 
+            // txtContacto
+            // 
+            this.txtContacto.Location = new System.Drawing.Point(189, 37);
+            this.txtContacto.Name = "txtContacto";
+            this.txtContacto.Size = new System.Drawing.Size(84, 20);
+            this.txtContacto.TabIndex = 3;
+            this.txtContacto.TextChanged += new System.EventHandler(this.txtContacto_TextChanged);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(69, 135);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(74, 36);
+            this.btnCancelar.TabIndex = 4;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(193, 135);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(80, 36);
+            this.btnRegistrar.TabIndex = 5;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // lstResultado
+            // 
+            this.lstResultado.FormattingEnabled = true;
+            this.lstResultado.Location = new System.Drawing.Point(69, 208);
+            this.lstResultado.Name = "lstResultado";
+            this.lstResultado.Size = new System.Drawing.Size(236, 56);
+            this.lstResultado.TabIndex = 6;
+            // 
+            // lblCantidadCon
+            // 
+            this.lblCantidadCon.AutoSize = true;
+            this.lblCantidadCon.Location = new System.Drawing.Point(53, 293);
+            this.lblCantidadCon.Name = "lblCantidadCon";
+            this.lblCantidadCon.Size = new System.Drawing.Size(77, 13);
+            this.lblCantidadCon.TabIndex = 7;
+            this.lblCantidadCon.Text = "CantContactos";
+            // 
+            // lblFechayHora
+            // 
+            this.lblFechayHora.AutoSize = true;
+            this.lblFechayHora.Location = new System.Drawing.Point(284, 296);
+            this.lblFechayHora.Name = "lblFechayHora";
+            this.lblFechayHora.Size = new System.Drawing.Size(71, 13);
+            this.lblFechayHora.TabIndex = 8;
+            this.lblFechayHora.Text = "Fecha y Hora";
+            // 
+            // frmGestionAgenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 339);
+            this.Controls.Add(this.lblFechayHora);
+            this.Controls.Add(this.lblCantidadCon);
+            this.Controls.Add(this.lstResultado);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.txtContacto);
+            this.Controls.Add(this.mtbNumero);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.lblContacto);
+            this.Name = "frmGestionAgenda";
+            this.Text = "Agenda de Contactos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblContacto;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.MaskedTextBox mtbNumero;
+        private System.Windows.Forms.TextBox txtContacto;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.ListBox lstResultado;
+        private System.Windows.Forms.Label lblCantidadCon;
+        private System.Windows.Forms.Label lblFechayHora;
+    }
+}
+
---
+++ pryAgendaContactoBenjaminDiaz/GestionAgenda.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryAgendaContactoBenjaminDiaz
+{
+    public partial class frmGestionAgenda : Form
+    {
+        public frmGestionAgenda()
+        {
+            InitializeComponent();
+        }
+
+        private void txtContacto_TextChanged(object sender, EventArgs e)
+        {
+            if (txtContacto.Text == "")
+            {
+                mtbNumero.Enabled = false;
+            }
+            else
+            {
+                mtbNumero.Enabled = true;
+            }
+            
+        }
+
+        private void mtbNumero_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            if (mtbNumero.Text != "")
+            {
+                btnRegistrar.Enabled = false;
+            }
+            else
+            {
+                btnRegistrar.Enabled = true;
+            }
+        }
+
+        string vContacto;
+        string vNumero;
+        string vContador;
+        string vFecha;
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            vContacto = txtContacto.Text;
+            vNumero = mtbNumero.Text;
+            lstResultado.Items.Add("Contacto"+vContacto+"Numero"+vNumero);
+            txtContacto.Text = "";
+            mtbNumero.Text = "";
+            vContador = vContador+1;
+            lblCantidadCon.Text = "Cantidad de contactos: "+vContador;
+            lblFechayHora.Text = "Fecha y hora:" +vFecha;
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtContacto.Text = "";
+            mtbNumero.Text = "";
+        }
+    }
+}
